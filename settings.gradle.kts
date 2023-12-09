5b226c673ea0de42a470364c278a736cacfa3768
Genesis <loda.tagulao.up@phinmaed.com>
2023-12-09 20:32:47 +0800

    test

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "GenesisTagulao-Block6-Quiz"
+include(":app")
